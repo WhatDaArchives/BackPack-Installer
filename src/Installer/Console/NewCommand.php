3e03d3796f0455d7e6de9901bdf2e38452896a12
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-03-12 03:24:53 -0700

    Initial commit

---
+++ src/Installer/Console/NewCommand.php
@@ -0,0 +1,14 @@
+<?php  namespace Installer\Installer\Console; 
+
+/**
+ * Class NewCommand
+ * @package Installer\Installer\Console
+ * @author Valentin PRUGNAUD <valentin@whatdafox.com>
+ * @url http://www.foxted.com
+ */
+class NewCommand {
+
+    
+
+    
+}

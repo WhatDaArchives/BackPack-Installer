64bf1e49fb1ca21d422bc287f0ab122f975861bf
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-03-12 03:45:59 -0700

    Add missing Process dependency

--- src/Installer/Console/NewCommand.php
+++ src/Installer/Console/NewCommand.php
@@ -1,10 +1,11 @@
 <?php  namespace BackPack\Installer\Console;
 
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use ZipArchive;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class NewCommand
